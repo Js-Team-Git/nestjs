c2c89077a08c6584502a02404f8bea3f1b1048a3
haveashish <ashish_singh@21@outlook.com>
2022-11-21 14:22:11 +0530

    mysql-client added in dockerfile

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,7 @@ WORKDIR /app
 ADD package.json /app/package.json
 RUN npm cache clean --force
 RUN npm install
+RUN apt-get install default-mysql-client -y
 ADD . /app
 EXPOSE 1401
 CMD ["npm", "run", "start"]
--- src/app.module.ts
+++ src/app.module.ts
@@ -9,7 +9,7 @@ import { UsersModule } from './users/users.module';
   imports: [
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: '10.10.10.27',
+      host: 'localhost',
       port: 3306,
       username: 'root',
       password: 'root123',
