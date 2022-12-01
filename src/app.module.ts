588637699eb30ec19217f17226cb726e79345ae8
Ashish Kumar Singh <ashishkumars@IMSS-JK-LAP-184.imsspl.com>
2022-12-01 12:43:47 +0530

    changes in app.module.ts for different  database configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,9 +7,26 @@ import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
+
+    ////PostgreSQL DB Congiguration////
+
+    // TypeOrmModule.forRoot({
+    //   type: 'postgres',
+    //   host: 'localhost',
+    //   port: 5432,
+    //   username: 'postgres',
+    //   password: 'root123',
+    //   database: 'users',
+    //   entities: [User],
+    //   synchronize: true
+    // }),
+
+    ////MySQL DB Configuration////
+    
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: 'host.docker.internal',
+      host: 'localhost',
+      // host: 'host.docker.internal',
       port: 3306,
       username: 'root',
       password: 'root123',
@@ -17,6 +34,22 @@ import { UsersModule } from './users/users.module';
       entities: [User],
       synchronize: true
     }),
+
+    ////MSSQL DB Configuration////
+
+    // TypeOrmModule.forRoot({
+    //   type: 'mssql',
+    //   host: 'localhost',
+    //   port: 1433,
+    //   username: 'sa',
+    //   password: 'int123$%^',
+    //   database: 'users',
+    //   entities: [User],
+    //   synchronize: true,
+    //   extra: {
+    //     trustServerCertificate: true,
+    //   }
+    // }),
     UsersModule],
   controllers: [AppController],
   providers: [AppService],
--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,7 +1,7 @@
 import { IsString } from "class-validator"
 
 export class CreateUserDto {
-    @IsString()
+    @IsString({})
     firstName: string
 
     @IsString()
